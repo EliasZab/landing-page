cf9476fa45d687c3e0c641211d5ff1b329808934
EliasCorr1998 <ezabaleta98@gmail.com>
2022-04-26 18:52:57 -0500

    primer commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,329 @@
  <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <div>
+    <!--  ////////////////////// HEADER ////////////////////////  -->
+    <v-row>
+      <v-col cols="12" md="2">
+        <v-img
+          class="img"
+          max-height="100%"
+          max-width="100%"
+          width="100%"
+          src="./images/OLIVOS PNG GRAND 1.png"
+        ></v-img>
+      </v-col>
+      
+      <v-col cols="12" md="3">
+
+        <v-row>
+          <v-col>
+            <a>
+              <v-img
+                class="img ma-auto"
+                max-height="100%"
+                max-width="100%"
+                width="100%"
+                src="./images/Internet (1).png"
+              ></v-img>
+            </a>
+          </v-col>
+
+          <v-col>
+            <a>
+              <v-img
+                class="img ma-auto"
+                max-height="100%"
+                max-width="100%"
+                width="100%"
+                src="./images/Instagram Circle.png"
+              ></v-img>
+            </a>
+          </v-col>
+
+          <v-col>
+            <a>
+              <v-img
+                class="img ma-auto"
+                max-height="100%"
+                max-width="100%"
+                width="100%"
+                src="./images/Facebook.png"
+              ></v-img>
+            </a>
+          </v-col>
+
+        </v-row>
+
+      </v-col>
+    </v-row>
+      <hr class="hr1" size="40">
+
+    <v-row class="ma-1">
+      <v-col cols="12" md="12">
+          <h1>Lorem ipsum</h1>
+          <h3>Lorem ipsum dolor sit amet, dolor sit amet</h3>
+      </v-col>
+
+    <!--  ////////////////////// INPUTS ////////////////////////  -->
+
+      <v-row>
+        <v-col class="ml-16 mr-10 mt-16" cols="12" md="5">
+          <div class="div-inputs">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum Lorem ipsum</p>
+            <v-text-field
+            label="Lorem ipsum Lorem ipsum  "
+            outlined
+            ></v-text-field>
+            <v-text-field
+            label="Lorem ipsum Lorem ipsum  "
+            outlined
+            ></v-text-field>
+            <v-text-field
+            label="Lorem ipsum Lorem ipsum  "
+            outlined
+            ></v-text-field>
+            <v-text-field
+            label="Lorem ipsum Lorem ipsum  "
+            outlined>
+            </v-text-field>
+
+            <v-checkbox
+              :label="`Acepto los términos y condiciones`"
+            ></v-checkbox>
+
+            <v-btn
+              color="#FF4E17"
+              dark
+              width="90%"
+              height="40px"
+            >
+              Lorem ipsum 
+            </v-btn>
+
+          </div>
+        </v-col>
+
+        <v-col class="ml-10 mr-16 mt-16" cols="12" md="5">
+          <v-img
+          class="img ma-auto"
+          max-height="100%"
+          max-width="100%"
+          width="100%"
+          src="./images/FAMILIA MANUAL 1.png"
+          ></v-img>
+
+          <p class="parrafo">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            sit amet, consectetur adipiscing elit. 
+          </p>
+        </v-col>
+      </v-row>
+    </v-row>
+
+    <!--  ////////////////////// SOMOS ////////////////////////  -->
+
+    <div class="div-Somos">
+      <v-row>
+        <v-col class="mt-16 mb-10" cols="12" md="12">
+          <h3>Somos tu mejor opcion en la protección exequial de tu familia, aquí te contamos por qué:</h3>
+        </v-col>
+        <v-col>
+          <v-row>
+            <v-col class="ma-auto" cols="12" md="2">
+              <div class="div-Pray">
+                <v-img
+                  class="img"
+                  max-height="100%"
+                  max-width="100%"
+                  width="50%"
+                  src="./images/Pray.png"
+                ></v-img>
+              </div>
+              <p>Lorem ipsum dolor sit amet, dolor sit amet</p>
+            </v-col>
+
+            <v-col class="ma-auto" cols="12" md="2">
+              <div class="div-Pray">
+                <v-img
+                  class="img"
+                  max-height="100%"
+                  max-width="100%"
+                  width="50%"
+                  src="./images/Pray.png"
+                ></v-img>
+              </div>
+              <p>Lorem ipsum dolor sit amet, dolor sit amet</p>
+            </v-col>
+
+            <v-col class="ma-auto" cols="12" md="2">
+              <div class="div-Pray">
+                <v-img
+                  class="img"
+                  max-height="100%"
+                  max-width="100%"
+                  width="50%"
+                  src="./images/Pray.png"
+                ></v-img>
+              </div>
+              <p>Lorem ipsum dolor sit amet, dolor sit amet</p>
+            </v-col>
+
+          </v-row>
+        </v-col>
+      </v-row>
+    </div>
+    <!--  ////////////////////// VECTOR ////////////////////////  -->
 
     <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      class="img ma-auto"
+      max-height="100%"
+      max-width="100%"
+      width="100%"
+      src="./images/vector.png"
     ></v-img>
 
-    <v-card-title>Cafe Badilico</v-card-title>
+    <!--  ////////////////////// QUE PLAN ////////////////////////  -->
 
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
+    <v-row>
+      <v-col class="mt-16" cols="12" md="12">
+        <h1>¿Que plan de protección escoges?</h1>
+        <h3>Contamos con una variedad de planes exequiales según tus necesidades:</h3>
+      </v-col>
 
-        <div class="grey--text ms-4">4.5 (413)</div>
-      </v-row>
+      <v-col class="mb-16">
+        <v-row>
+          <v-col class="ma-auto" cols="12" md="4">
+            <div class="div-inputs">
+              <v-img
+                class="img"
+                max-height="100%"
+                max-width="70%"
+                width="100%"
+                src="./images/Mesa de trabajo 1 1.png"
+              ></v-img>
+              <h1>Planes Empresas</h1>
+            </div>
+          </v-col>
 
-      <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+          <v-col class="ma-auto" cols="12" md="4">
+            <div class="div-inputs">
+              <v-img
+                class="img"
+                max-height="100%"
+                max-width="70%"
+                width="100%"
+                src="./images/Mesa de trabajo 1 copia 1.png"
+              ></v-img>
+              <h1>Planes Personas</h1>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
 
-      <div>
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
-      </div>
-    </v-card-text>
+     <v-img
+      class="img ma-auto"
+      max-height="100%"
+      max-width="100%"
+      width="100%"
+      src="./images/Vector 2.png"
+    ></v-img>   
 
-    <v-divider class="mx-4"></v-divider>
+    <!--  ////////////////////// SUMERGÉTE ////////////////////////  -->
 
-    <v-card-title>Tonight's availability</v-card-title>
+    <v-row class="mb-16">
+      <v-col class="mt-16" cols="12" md="12">
+        <h1>Sumergéte en un mundo de protección y muchos beneficios</h1>
+        <h3>Afiliandote desde aquí, reclama tu tarjeta beneficios con cobertura inmediata</h3>
+        <v-img
+          class="img ma-auto"
+          max-height="100%"
+          max-width="25%"
+          width="100%"
+          src="./images/Rectangle 8.png"
+        ></v-img>
+      </v-col>
+      <v-img
+        class="img ma-auto"
+        max-height="100%"
+        max-width="100%"
+        width="100%"
+        src="./images/Vector (1).png"
+      ></v-img>
+    </v-row>
 
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
+    <hr class="hr1" size="30">
 
-        <v-chip>7:30PM</v-chip>
+    <!--  ////////////////////// FOOTER ////////////////////////  -->
 
-        <v-chip>8:00PM</v-chip>
+    <v-row>
+      <v-col class="mt-1 mb-1">
+        <h1 class="h1-footer">© 2022 Digital Alliance</h1>
+      </v-col>
+    </v-row>
 
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
+  </div>
+</template>
 
-    <v-card-actions>
-      <v-btn color="deep-purple lighten-2" text @click="reserve">
-        Reserve
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</template><script>
+<script>
 export default {
-  data: () => ({
-    loading: false,
-    selection: 1,
-  }),
 
-  methods: {
-    reserve() {
-      this.loading = true;
-
-      setTimeout(() => (this.loading = false), 2000);
-    },
-  },
 };
 </script>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+.hr1{
+  height: 3px;
+  background: rgb(219, 219, 219);
+  border: none;
+}
+.div-inputs{
+  border-radius: 5%;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+}
+h1, h2{
+  color: #FF4E17;
+}
+h3{
+  font-size: 30px;
+  color: black;
+}
+p{
+  color: black;
+}
+.parrafo{
+  font-size: 30px;
+  text-align: justify;
+}
+.div-Somos{
+  background: #FF4E17;
+}
+.div-Somos h3{
+  color: white;
+  margin-left: 20%;
+  margin-right: 20%;
+  text-align: center;
+}
+.div-Pray{
+  background: white;
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+}
+.div-Somos p{
+  color: white;
+  font-size: 25px;
+}
+.h1-footer{
+  color: #A9A2A2;
+  font-size: 25px;
+  font-weight: 400;
+}
+</style>
